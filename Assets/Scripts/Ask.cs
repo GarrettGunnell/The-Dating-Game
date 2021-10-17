c469c85e485fef8a3845bca110bdd19a33e43070
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-16 18:28:45 -0700

    Detect mouse click on ask button

---
+++ Assets/Scripts/Ask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ask : MonoBehaviour {
+
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.name == "Ask") {
+                    Debug.Log("Ask");
+                }
+            }
+        }
+    }
+}
