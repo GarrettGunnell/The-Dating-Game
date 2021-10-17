8b6fdfb0f69b0acab6a8f0c229d87bb9d243d46e
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-17 16:04:12 -0700

    Log chosen option

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -25,16 +25,23 @@ public class StateManager : MonoBehaviour {
                     if (hit.transform.name == "Talk") {
                         Debug.Log("Talk");
                         optionsManager.Talk();
+                        //setIdle(false);
+                        talking = true;
                     } else if (hit.transform.name == "Ask") {
                         Debug.Log("Ask");
                         optionsManager.Ask();
+                        //setIdle(false);
+                        talking = true;
                     } else if (hit.transform.name == "Girl") {
                         Debug.Log("Girl");
                     }
                 } else if (collided && talking) {
                     if (hit.transform.name.Contains("Option")) {
-                        string chosenOption = hit.transform.name.Split(' ')[1];
-                        Debug.Log(chosenOption);
+                        int chosenOption = int.Parse(hit.transform.name.Split(' ')[1]);
+                        
+                        Debug.Log("Chose option: " + chosenOption);
+                        Debug.Log(optionsManager.getOption(chosenOption - 1));
+                        setIdle(false);
                     }
                 }
             }
