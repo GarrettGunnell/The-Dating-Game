da10b8a1cc87c9431080a66f3042a2264b47ee7e
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-16 18:19:43 -0700

    Detect mouse click on talk button

--- Assets/Scripts/Talk.cs
+++ Assets/Scripts/Talk.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Talk : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Talk : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.name == "Talk") {
+                    Debug.Log("Talk");
+                }
+            }
+        }
     }
 }
