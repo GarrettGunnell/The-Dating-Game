12683156e5d6a9e2ff5ca0ef4e1da07d49d59582
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-16 19:15:18 -0700

    Detect when hovering over girl

---
+++ Assets/Scripts/Girl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : MonoBehaviour {
+
+    void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.name == "Girl") {
+                Debug.Log("Girl");
+            }
+        }
+    }
+}
