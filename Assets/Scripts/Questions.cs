6747af89588e20627c8da0fc349b50f4c6fb0f38
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-17 02:23:36 -0700

    Generate and return 6 dummy questions from questions class

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -6,4 +6,17 @@ public class Questions {
     public Questions() {
         
     }
+
+    public List<string> generateQuestions() {
+        List<string> qs = new List<string>();
+
+        qs.Add("What's your name?");
+        qs.Add("What do you like to do in your free time?");
+        qs.Add("What's a movie you really enjoyed?");
+        qs.Add("Do you have any pets?");
+        qs.Add("Have you ever been out of the country?");
+        qs.Add("Are you a morning person?");
+
+        return qs;
+    }
 }
