afca85368cb682fb2dc50535725a1ad47c0af5f6
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-17 19:50:07 -0700

    Attach knowledge piece as second element of response list

--- Assets/Scripts/Girl.cs
+++ Assets/Scripts/Girl.cs
@@ -10,11 +10,11 @@ public class Girl : MonoBehaviour {
         questionResponses = new Hashtable();
 
         questionResponses.Add("What's your name?", new List<string> {"Did you really forget my name?", null});
-        questionResponses.Add("What do you like to do in your free time?", new List<string> {"Did you really forget my name?", null});
-        questionResponses.Add("What's a movie you really enjoyed?", new List<string> {"Did you really forget my name?", null});
-        questionResponses.Add("Do you have any pets?", new List<string> {"Did you really forget my name?", null});
-        questionResponses.Add("Have you ever been out of the country?", new List<string> {"Did you really forget my name?", null});
-        questionResponses.Add("Are you a morning person?", new List<string> {"Did you really forget my name?", null});
+        questionResponses.Add("What do you like to do in your free time?", new List<string> {"I like making music, perhaps you've heard of my band.", "music"});
+        questionResponses.Add("What's a movie you really enjoyed?", new List<string> {"My favorite movie is Drive.", "Drive"});
+        questionResponses.Add("Do you have any pets?", new List<string> {"Yes I have a pet bird!", "bird"});
+        questionResponses.Add("Have you ever been out of the country?", new List<string> {"I've toured all over the world!", "toured"});
+        questionResponses.Add("Are you a morning person?", new List<string> {"No I prefer the solitude of night time.", "night"});
     
     }
 
