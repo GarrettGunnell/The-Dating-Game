4afa25619fd78dca5f122d93e7908a3f04fa370a
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-19 01:40:48 -0700

    Create a basic hashset containing a few initial questions

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 
 public class Questions {
 
+    private HashSet<string> allQuestions;
     private HashSet<string> askedQuestions;
 
     public Questions() {
         askedQuestions = new HashSet<string>();
+        allQuestions = new HashSet<string>();
+
+        allQuestions.Add("So, what's your name?");
+        allQuestions.Add("What do you like doing with your free time?");
+        allQuestions.Add("Watched any cool movies lately?");
+        allQuestions.Add("Do you have any pets?");
+        allQuestions.Add("Have you traveled anywhere interesting?");
+        allQuestions.Add("Are you a morning person?");
     }
 
     public void AddAskedQuestion(string q) {
