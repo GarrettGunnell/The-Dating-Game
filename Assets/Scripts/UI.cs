5bc0f58db4459d6837414873bdca4fffb25fa7c1
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-22 23:45:20 -0700

    Use custom cursor texture

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    public Texture2D mouseCursor;
+
+    Vector2 hotSpot = new Vector2(0, 0);
+    CursorMode cursorMode = CursorMode.Auto;
+
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.SetCursor(mouseCursor, hotSpot, cursorMode);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
