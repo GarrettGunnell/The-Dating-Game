99bf26168fbc731e917d72baaf4ab898b108467b
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-23 00:44:32 -0700

    Change leave button texture on hover

---
+++ Assets/Scripts/Leave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leave : MonoBehaviour {
+    public Texture2D defaultTex, hoverTex, disabledTex;
+
+    private bool isEnabled;
+    private Material material;
+
+    void Start() {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update() {
+
+    }
+
+    void OnMouseOver() {
+        material.SetTexture("_MainTex", hoverTex);
+    }
+
+    void OnMouseExit() {
+        material.SetTexture("_MainTex", defaultTex);
+    }
+}
