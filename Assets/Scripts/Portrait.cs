7d0b10561405237d140975c01f57a3a8eb453803
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-23 05:02:56 -0700

    Move portrait around with mouse

---
+++ Assets/Scripts/Portrait.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portrait : MonoBehaviour {
+
+    float x, y;
+    Vector3 pos;
+    
+    void Start() {
+        pos = transform.position;
+    }
+
+    void Update() {
+        float targetX = Input.mousePosition.x - Screen.width * 0.75f;
+        float dx = targetX - x;
+        x += dx * 0.05f;
+        float targetY = Input.mousePosition.y - Screen.height / 2.0f;
+        float dy = targetY - y;
+        y += dy * 0.05f;
+
+        Vector3 direction = new Vector3(x, y, 0.0f);
+        direction.y *= 1 / 500.0f * 0.2f;
+        direction.x *= 1 / 500.0f * 0.7f;
+        this.transform.position = pos - direction;
+    }
+}
