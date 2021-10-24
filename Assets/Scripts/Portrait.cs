13939bdcd9a41972e5ecfcc030d27e39f3e274fc
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-24 03:50:03 -0700

    Prevent portrait movement if mouse exceeds window bounds

--- Assets/Scripts/Portrait.cs
+++ Assets/Scripts/Portrait.cs
@@ -12,16 +12,18 @@ public class Portrait : MonoBehaviour {
     }
 
     void Update() {
-        float targetX = Input.mousePosition.x - Screen.width * 0.75f;
-        float dx = targetX - x;
-        x += dx * 0.05f;
-        float targetY = Input.mousePosition.y - Screen.height / 2.0f;
-        float dy = targetY - y;
-        y += dy * 0.05f;
+        if ( 0 < Input.mousePosition.x && Input.mousePosition.x < Screen.width && 0 < Input.mousePosition.y && Input.mousePosition.y < Screen.height) {
+            float targetX = Input.mousePosition.x - Screen.width * 0.75f;
+            float dx = targetX - x;
+            x += dx * 0.05f;
+            float targetY = Input.mousePosition.y - Screen.height / 2.0f;
+            float dy = targetY - y;
+            y += dy * 0.05f;
 
-        Vector3 direction = new Vector3(x, y, 0.0f);
-        direction.y *= 1 / 500.0f * 0.2f;
-        direction.x *= 1 / 500.0f * 0.7f;
-        this.transform.position = pos - direction;
+            Vector3 direction = new Vector3(x, y, 0.0f);
+            direction.y *= 1 / 500.0f * 0.2f;
+            direction.x *= 1 / 500.0f * 0.7f;
+            this.transform.position = pos - direction;
+        }
     }
 }
