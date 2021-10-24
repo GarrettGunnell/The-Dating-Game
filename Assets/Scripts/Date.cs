eb8b784a11d0f199f49116c69429a9173af1870d
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-24 06:41:11 -0700

    Load game on Date press

--- Assets/Scripts/Date.cs
+++ Assets/Scripts/Date.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Date : MonoBehaviour {
 
@@ -27,5 +28,6 @@ public class Date : MonoBehaviour {
 
     void OnMouseDown() {
         audioSource.Play();
+        SceneManager.LoadScene("MainGame");
     }
 }
