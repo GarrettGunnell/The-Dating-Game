1e0b19da6edc7b799ca4811a90a06aa0ad10b065
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-24 06:30:31 -0700

    Change cursor when hovering over main menu buttons

---
+++ Assets/Scripts/Flake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flake : MonoBehaviour {
+    public Texture2D defaultTex, hoverTex;
+
+    private Material material;
+
+    void Start() {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void OnMouseOver() {
+        material.SetTexture("_MainTex", hoverTex);
+        MainMenu.changeCursor(true);
+    }
+
+    void OnMouseExit() {
+        material.SetTexture("_MainTex", defaultTex);
+        MainMenu.changeCursor(false);
+    }
+}
