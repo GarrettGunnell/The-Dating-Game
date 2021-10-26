5bd9cb987248b7cee074ab45d6ac62e176a8c6b1
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-25 17:03:05 -0700

    Implement opening credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Credits : MonoBehaviour {
+
+    public VideoPlayer vidPlayer;
+    public ColorSchemer colorSchemer;
+
+    void Start() {
+    }
+
+    void Update() {
+        if (vidPlayer.frame > 0 && vidPlayer.isPlaying == false) {
+            colorSchemer.enabled = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
