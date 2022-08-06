8a8f8ed582f43207c57806ac77dec1ed32cd0728
Aliaksei Kalosha <aliaksei.kalosha@tul.cz>
2022-08-06 22:20:24 +0200

    move colour scheme data to ScriptableObject

---
+++ Assets/Scripts/UI/ColorSchemeData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorSchemeData", menuName = "ScriptableObjects/ColorSchemeData", order = 1)]
+public class ColorSchemeData : ScriptableObject
+{
+    [Serializable]
+    public class Data
+    {
+        [SerializeField] private Color brighter = Color.white;
+        [SerializeField] private Color darker = Color.black;
+
+        private Vector4 ToVec4(Color color)
+        {
+            Func<float, float> f = (a) => Mathf.Floor(a * 256);
+            return new Vector4(f(color.r), f(color.g), f(color.b), f(color.a));
+        }
+
+        public Data(Vector4 d, Vector4 b)
+        {
+            b /= 255;
+            d /= 255;
+            brighter = new Color(b.x, b.y, b.z, b.w);
+            darker = new Color(d.x, d.y, d.z, d.w);
+        }
+
+        public Vector4 Brighter => ToVec4(brighter);
+        public Vector4 Darker => ToVec4(darker);
+    }
+    [SerializeField] private Data[] colorSchemes;
+
+    public int Count => colorSchemes.Length;
+    public Data this[int i] => colorSchemes[i];
+}
--- Assets/Scripts/UI/ColorSchemer.cs
+++ Assets/Scripts/UI/ColorSchemer.cs
@@ -1,145 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Camera))]
 public class ColorSchemer : MonoBehaviour {
+
     [SerializeField] private Shader colorSchemeShader;
+    [SerializeField] private ColorSchemeData colorSchemes;
 
     private Material colorSchemeMat;
 
-    private List<Vector4[]> colorSchemes = null;
     private static int schemeIndex = -1;
 
     void Start() {
-        colorSchemes = new List<Vector4[]>();
-
-        Vector4[] scheme1 = new Vector4[] {
-            new Vector4(21, 22, 18, 255),
-            new Vector4(232, 225, 225, 255)
-        };
-        
-        Vector4[] scheme2 = new Vector4[] {
-            new Vector4(62, 35, 44, 255),
-            new Vector4(237, 246, 214, 255)
-        };
-
-        Vector4[] scheme3 = new Vector4[] {
-            new Vector4(18, 18, 23, 255),
-            new Vector4(232, 246, 214, 255)
-        };
-
-        Vector4[] scheme4 = new Vector4[] {
-            new Vector4(15, 10, 14, 255),
-            new Vector4(240, 246, 240, 255)
-        };
-
-        Vector4[] scheme5 = new Vector4[] {
-            new Vector4(38, 31, 96, 255),
-            new Vector4(189, 231, 193, 255)
-        };
-
-        Vector4[] scheme6 = new Vector4[] {
-            new Vector4(26, 24, 11, 255),
-            new Vector4(246, 217, 81, 255)
-        };
-        
-        Vector4[] scheme7 = new Vector4[] {
-            new Vector4(14, 16, 14, 255),
-            new Vector4(200, 40, 45, 255)
-        };
-
-        Vector4[] scheme8 = new Vector4[] {
-            new Vector4(9, 8, 11, 255),
-            new Vector4(170, 20, 45, 255)
-        };
-
-        Vector4[] scheme9 = new Vector4[] {
-            new Vector4(35, 11, 12, 255),
-            new Vector4(255, 249, 225, 255)
-        };
-        
-        Vector4[] scheme10 = new Vector4[] {
-            new Vector4(27, 27, 30, 255),
-            new Vector4(232, 184, 122, 255)
-        };
-
-        Vector4[] scheme11 = new Vector4[] {
-            new Vector4(24, 25, 22, 255),
-            new Vector4(213, 117, 206, 255)
-        };
-
-        Vector4[] scheme12 = new Vector4[] {
-            new Vector4(116, 11, 16, 255),
-            new Vector4(135, 255, 198, 255)
-        };
-        
-        Vector4[] scheme13 = new Vector4[] {
-            new Vector4(18, 72, 56, 255),
-            new Vector4(255, 174, 52, 255)
-        };
-
-        Vector4[] scheme14 = new Vector4[] {
-            new Vector4(52, 56, 52, 255),
-            new Vector4(255, 204, 167, 255)
-        };
-
-        Vector4[] scheme15 = new Vector4[] {
-            new Vector4(10, 5, 50, 255),
-            new Vector4(251, 182, 209, 255)
-        };
-
-        Vector4[] scheme16 = new Vector4[] {
-            new Vector4(15, 87, 143, 255),
-            new Vector4(251, 182, 209, 255)
-        };
-
-        Vector4[] scheme17 = new Vector4[] {
-            new Vector4(36, 54, 104, 255),
-            new Vector4(122, 127, 123, 255)
-        };
-
-        Vector4[] scheme18 = new Vector4[] {
-            new Vector4(109, 61, 15, 255),
-            new Vector4(152, 178, 18, 255)
-        };
-
-        Vector4[] scheme19 = new Vector4[] {
-            new Vector4(79, 42, 70, 255),
-            new Vector4(152, 178, 118, 255)
-        };
-        
-        Vector4[] scheme20 = new Vector4[] {
-            new Vector4(131, 45, 56, 255),
-            new Vector4(229, 214, 209, 255)
-        };
-
-        Vector4[] scheme21 = new Vector4[] {
-            new Vector4(90, 56, 51, 255),
-            new Vector4(221, 141, 130, 255)
-        };
-
-        colorSchemes.Add(scheme1);
-        colorSchemes.Add(scheme2);
-        colorSchemes.Add(scheme3);
-        colorSchemes.Add(scheme4);
-        colorSchemes.Add(scheme5);
-        colorSchemes.Add(scheme6);
-        colorSchemes.Add(scheme7);
-        colorSchemes.Add(scheme8);
-        colorSchemes.Add(scheme9);
-        colorSchemes.Add(scheme10);
-        colorSchemes.Add(scheme11);
-        colorSchemes.Add(scheme12);
-        colorSchemes.Add(scheme13);
-        colorSchemes.Add(scheme14);
-        colorSchemes.Add(scheme15);
-        colorSchemes.Add(scheme16);
-        colorSchemes.Add(scheme17);
-        colorSchemes.Add(scheme18);
-        colorSchemes.Add(scheme19);
-        colorSchemes.Add(scheme20);
-        colorSchemes.Add(scheme21);
 
         if (schemeIndex == -1)
             schemeIndex = Random.Range(0, colorSchemes.Count);
@@ -151,8 +23,9 @@ public class ColorSchemer : MonoBehaviour {
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
-        colorSchemeMat.SetVector("col1", colorSchemes[schemeIndex][0]);
-        colorSchemeMat.SetVector("col2", colorSchemes[schemeIndex][1]);
+        var colors = colorSchemes[schemeIndex];
+        colorSchemeMat.SetVector("col1", colors.Darker);
+        colorSchemeMat.SetVector("col2", colors.Brighter);
         Graphics.Blit(source, destination, colorSchemeMat);
     }
 
